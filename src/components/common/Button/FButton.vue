27a0b581c2e7db44456a77092b8d4a3491017bf2
Nguyễn Văn Hằng <hangnguyen19091991@gmail.com>
2022-12-09 09:47:35 +0700

    them icon

---
+++ src/components/common/Button/FButton.vue
@@ -0,0 +1,118 @@
+<template>
+    <button :type="btnType">
+        <span class="shadow"></span>
+        <span class="edge"></span>
+        <span class="front text"><img v-if="icon" src="@/assets/images/icon/icon_edit.svg" alt="" class="me-2"> {{ $t(title) }} </span>
+    </button>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    props: {
+        btnType: {
+            type: String,
+            default: "button"
+        },
+        title: {
+            type: String,
+            default: "Click"
+        },
+        icon: {
+            type: Boolean,
+            required: false
+        }
+    },
+})
+</script>
+<style lang="scss" scoped>
+button {
+    position: relative;
+    border: none;
+    background: transparent;
+    padding: 0;
+    cursor: pointer;
+    outline-offset: 4px;
+    transition: filter 250ms;
+    user-select: none;
+    touch-action: manipulation;
+
+    &:hover {
+        filter: brightness(110%);
+
+        .front {
+            transform: translateY(-6px);
+            transition: transform
+                250ms
+                cubic-bezier(.3, .7, .4, 1.5);
+        }
+
+        .shadow {
+            transform: translateY(4px);
+            transition: transform
+                250ms
+                cubic-bezier(.3, .7, .4, 1.5);
+        }
+    }
+
+    .edge {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        border-radius: 12px;
+        background: linear-gradient(
+            to left,
+            hsl(340deg 100% 16%) 0%,
+            hsl(340deg 100% 32%) 8%,
+            hsl(340deg 100% 32%) 92%,
+            hsl(340deg 100% 16%) 100%
+        );
+    }
+
+    .shadow {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        border-radius: 12px;
+        background: hsl(0deg 0% 0% / 0.25);
+        will-change: transform;
+        transform: translateY(2px);
+        transition: transform
+            600ms
+            cubic-bezier(.3, .7, .4, 1);
+    }
+
+    .front {
+        display: block;
+        position: relative;
+        padding: 12px 27px;
+        border-radius: 12px;
+        font-size: 1.1rem;
+        color: white;
+        background: hsl(345deg 100% 47%);
+        will-change: transform;
+        transform: translateY(-4px);
+        transition: transform
+            600ms
+            cubic-bezier(.3, .7, .4, 1);
+    }
+}
+
+
+button:active .front {
+ transform: translateY(-2px);
+ transition: transform 34ms;
+}
+
+button:active .shadow {
+ transform: translateY(1px);
+ transition: transform 34ms;
+}
+
+button:focus:not(:focus-visible) {
+ outline: none;
+}
+</style>
---
+++ src/components/decorator/TheRightBar.vue
@@ -0,0 +1,97 @@
+<template>
+    <div>
+        <div class="rightbar py-3">
+            <div class="account__access">
+                <div class="label">{{ $t('label.fastAccess') }}</div>
+                <div class="access-items py-3">
+                    <div class="notification">
+                        <img src="@/assets/images/ring.png" width="20" height="20" alt="">
+                        <span v-if="numNotification">{{ numNotification }}</span>
+                        <span>{{ $t('label.notification') }}</span>
+                    </div>
+                    <div class="prioritized">
+                        <img src="@/assets/images/star.png" width="20" height="20" alt="">
+                        <span>{{ $t('label.created.prioritized') }}</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="account-network py-2">
+                <div class="account-network__head d-inline">
+                    <div class="label">{{ $t('label.friends') }}</div>
+                    <div class="account-network__actions">
+                        <div class="search"></div>
+                        <div class="multi-action">
+                            <img src="@/assets/images/multi_actions.png" width="24" height="24" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+    setup() {
+        const numNotification = ref(0);
+
+        return {
+            numNotification
+        }
+    }
+})
+</script>
+<style lang="scss" scoped>
+.rightbar {
+    height: 100vh;
+    position: relative;
+    width: 100%;
+    text-align: left;
+    padding-left: 50px;
+
+    span {
+        color: #777;
+    }
+
+    img {
+        opacity: .5;
+        margin-right: .75rem;
+    }
+
+    .label {
+        color: #777;
+        font-weight: 600;
+    }
+
+    .account__access {
+        border-bottom: 1px solid #ccc;
+        display: grid;
+
+        .access-items {
+            .notification, .prioritized {
+                padding: 8px 15px;
+                &:hover {
+                    border-radius: 5px;
+                    cursor: pointer;
+                    background-color: #d0d0d075;
+                }
+            }
+        }
+    }
+
+    .account-network {
+        .account-network__head {
+            display: inline-block;
+            .label {
+                display: block;
+                float: left;
+            }
+            .account-network__actions {
+                display: block;
+                float: right;
+            }
+        }
+    }
+}
+</style>
