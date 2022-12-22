967cc22fcb14515d482232833ffc57eb9c618fca
Nguyễn Văn Hằng <hangnguyen19091991@gmail.com>
2022-12-22 10:09:30 +0700

    thêm router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,20 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import {
+  LayoutEnum
+} from '@/constant/LayoutEnum'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('@/views/Home/index.vue')
+    component: () => import('@/views/Home/index.vue'),
+    meta: { layout: LayoutEnum.UserLayout }
   },
   {
     path: '/about',
     name: 'About',
-    component: () => import('@/views/AboutView.vue')
+    component: () => import('@/views/AboutView.vue'),
+    meta: { layout: LayoutEnum.UserLayout }
   },
   {
     path: '/exam',
@@ -18,29 +23,58 @@ const routes: Array<RouteRecordRaw> = [
       {
         path: '',
         name: 'ExamCommon',
-        component: () => import('@/views/Exam/index.vue')
+        component: () => import('@/views/Exam/index.vue'),
+        meta: { layout: LayoutEnum.UserLayout }
       },
       {
         path: ":cid",
         name: 'ExamPersonal',
-        component: () => import('@/views/Exam/index.vue')
-      }
+        component: () => import('@/views/Exam/index.vue'),
+        meta: { layout: LayoutEnum.UserLayout }
+      },
+      {
+        path: ":cid/personal",
+        name: 'ExamPersonalMore',
+        component: () => import('@/views/Exam/Personal/index.vue'),
+        meta: { layout: LayoutEnum.UserLayout }
+      },
+      {
+        path: ':cid/recents',
+        name: 'ExamRecents',
+        component: () => import('@/views/Recents/index.vue'),
+        meta: { layout: LayoutEnum.UserLayout }
+      },
     ]
   },
   {
     path: '/question',
     name: 'Question',
-    component: () => import('@/views/Source/Question.vue')
+    component: () => import('@/views/Source/Question.vue'),
+    meta: { layout: LayoutEnum.UserLayout }
   },
   {
     path: '/report',
     name: 'Report',
-    component: () => import('@/views/Report/index.vue')
+    component: () => import('@/views/Report/index.vue'),
+    meta: { layout: LayoutEnum.UserLayout }
   },
   {
     path: '/profile',
-    name: 'Profile',
-    component: () => import('@/views/Profile/index.vue')
+    // name: 'Profile',
+    children: [
+      {
+        path: '',
+        name: 'ProfileInformations',
+        component: () => import('@/views/Profile/Info/index.vue'),
+        meta: { layout: LayoutEnum.SettingLayout }
+      },
+      {
+        path: ':username',
+        name: 'ProfileEdit',
+        component: () => import('@/views/Profile/index.vue'),
+        meta: { layout: LayoutEnum.SettingLayout }
+      }
+    ]
   },
   {
     path: '/friends',
@@ -70,8 +104,38 @@ const routes: Array<RouteRecordRaw> = [
   },
   {
     path: '/setting',
-    name: 'Setting',
-    component: () => import('@/views/Setting/index.vue')
+    children: [
+      {
+        path: '',
+        name: 'Setting',
+        component: () => import('@/views/Setting/index.vue'),
+        meta: { layout: LayoutEnum.SettingLayout }
+      },
+      {
+        path: 'courses',
+        name: 'SettingCourse',
+        component: () => import('@/views/Setting/Courses/index.vue'),
+        meta: { layout: LayoutEnum.SettingLayout }
+      },
+      {
+        path: 'password',
+        name: 'SettingPassword',
+        component: () => import('@/views/Setting/Password/index.vue'),
+        meta: { layout: LayoutEnum.SettingLayout }
+      },
+      {
+        path: 'notifications',
+        name: 'SettingNotifications',
+        component: () => import('@/views/Setting/Notification/index.vue'),
+        meta: { layout: LayoutEnum.SettingLayout }
+      },
+      {
+        path: 'privacy',
+        name: 'SettingPrivacy',
+        component: () => import('@/views/Setting/Privacy/index.vue'),
+        meta: { layout: LayoutEnum.SettingLayout }
+      },
+    ]
   }
 ]
 
